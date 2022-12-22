0031b044de5532654b4cebd17399745ffa64b7d6
Julianex007 <120682438+Julianex007@users.noreply.github.com>
2022-12-22 14:45:07 -0300

    Add files via upload

---
+++ HTML Pruebas/pruebas.js
@@ -0,0 +1,22 @@
+let futbol=["pelota","jugador","pasto","Arco"];
+console.log(futbol);
+futbol.push("silbato");
+
+futbol.unshift("cancha");
+
+console.log(futbol);
+alert("Estoy enmezando a entender", futbol)
+
+function Bird(name) {
+    this.name = "name",
+    this.age= 1
+};
+Bird.prototype = {
+    wings: 2,
+    fly: function(){
+        return "I can Fly!";
+    }
+};
+
+let penguin = new Bird("Happy");
+console.log(penguin);
